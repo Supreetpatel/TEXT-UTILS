ac15f4a6f7476b7d1124a61439878e322f855506
Supreet0 <150177964+Supreetpatel@users.noreply.github.com>
2024-09-07 00:59:04 +0530

    CHANGES

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,13 @@ export default function Navbar(props) {
   return (
     <nav class={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Text-Utils</a>
+    <a class="navbar-brand" href="/">TEXT-UTILS APP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" to="/">Home</a>
-        </li>
-      </ul>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    </ul>
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode}  type="checkbox" role="switch" id="flexSwitchCheckDefault" />
       <label className="form-check-label" htmlForor="flexSwitchCheckDefault">Dark mode</label>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -45,7 +45,7 @@ import React,{useState} from 'react'
    </div>
    <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
     <h1>Your text summary</h1>
-    <p>{text.split(" ").filter((element)=>{return element.length !==0}).length} words and {text.length} characters</p>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length !==0}).length} words and {text.length} characters</p>
     <p>{0.008*text.split(" ").filter((element)=>{return element.length !==0}).length} minutes to read</p>
     <h2>Preview</h2>
     <p>{text}</p>
